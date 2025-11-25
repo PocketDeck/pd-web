4045526d80d9b02533a5318af3a67227abd93ffa
Kilian Chung <kilian.chung@web.de>
2025-11-25 13:40:16 +0100

    upd

--- core/main.mjs
+++ core/main.mjs
@@ -1,8 +1,6 @@
 import { initSocket } from '/core/socket.mjs';
 import { navigate } from '/core/router.mjs';
 
-// Create a global app context (or use something like a state manager)
-const socket = initSocket();
-
-//navigate(window.location.href, socket);
-navigate('/games/uno', socket);
+initSocket();
+//navigate(window.location.href);
+navigate('/games/uno');
